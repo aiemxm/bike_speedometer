e1c7b741f687d8a5ae3f701495b294bac8af0392
Maxime Bégué <maxime1.begue@gmail.com>
2022-12-02 18:28:18 +0100

    Initial Commit

---
+++ bike_speedometer/bike_speedometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  bike_speedometerApp.swift
+//  bike_speedometer
+//
+//  Created by Maxime Bégué on 02/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct bike_speedometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bike_speedometerUITests/bike_speedometerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  bike_speedometerUITestsLaunchTests.swift
+//  bike_speedometerUITests
+//
+//  Created by Maxime Bégué on 02/12/2022.
+//
+
+import XCTest
+
+final class bike_speedometerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
